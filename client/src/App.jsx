17b4ee3a84043c20a80c10b3a966b2900fe216b7
Anagha <anaghabharadwaj621@gmail.com>
2025-07-26 10:29:09 +0530

    Initialized React.js and TailwindCss setup

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-red-400 text-2xl p-5 items-center">
+      Hello, this is fake news detection website
+    </div>
+  );
+}
+
+export default App;
