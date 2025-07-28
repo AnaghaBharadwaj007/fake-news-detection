66a0b16af90e86fdfd6247406f6f06828be932a7
Anagha <anaghabharadwaj621@gmail.com>
2025-07-28 14:46:22 +0530

    Made the page transition smooth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 import About from "./Pages/About";
@@ -10,51 +12,176 @@ import Profile from "./Pages/Profile";
 import Signin from "./Pages/Signin";
 import SignUp from "./Pages/Signup";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/about" element={<About />} />
+// Define animation variants for page transitions
+const pageVariants = {
+  initial: {
+    opacity: 0,
+  },
+  in: {
+    opacity: 1,
+  },
+  out: {
+    opacity: 0,
+  },
+};
 
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/detect"
-          element={
-            <ProtectedRoute>
-              <Detection />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/history"
-          element={
-            <ProtectedRoute>
-              <History />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+// Define transition properties
+const pageTransition = {
+  type: "tween",
+  ease: "easeInOut",
+  duration: 0.3,
+};
+
+function AppContent() {
+  const location = useLocation();
+
+  return (
+    <>
+      <Navbar />
+      {/* AnimatePresence allows components to animate when they are removed from the React tree */}
+      <AnimatePresence mode="wait">
+        {/* The key and location props are crucial for AnimatePresence to detect route changes */}
+        <Routes location={location} key={location.pathname}>
+          <Route
+            path="/"
+            element={
+              <motion.div
+                initial="initial"
+                animate="in"
+                exit="out"
+                variants={pageVariants}
+                transition={pageTransition}
+                style={{ width: "100%" }}
+              >
+                <Home />
+              </motion.div>
+            }
+          />
+          <Route
+            path="/signin"
+            element={
+              <motion.div
+                initial="initial"
+                animate="in"
+                exit="out"
+                variants={pageVariants}
+                transition={pageTransition}
+                style={{ width: "100%" }}
+              >
+                <Signin />
+              </motion.div>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <motion.div
+                initial="initial"
+                animate="in"
+                exit="out"
+                variants={pageVariants}
+                transition={pageTransition}
+                style={{ width: "100%" }}
+              >
+                <SignUp />
+              </motion.div>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <motion.div
+                initial="initial"
+                animate="in"
+                exit="out"
+                variants={pageVariants}
+                transition={pageTransition}
+                style={{ width: "100%" }}
+              >
+                <About />
+              </motion.div>
+            }
+          />
+
+          {/* Protected Routes */}
+          <Route
+            path="/dashboard"
+            element={
+              <motion.div
+                initial="initial"
+                animate="in"
+                exit="out"
+                variants={pageVariants}
+                transition={pageTransition}
+                style={{ width: "100%" }}
+              >
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              </motion.div>
+            }
+          />
+          <Route
+            path="/detect"
+            element={
+              <motion.div
+                initial="initial"
+                animate="in"
+                exit="out"
+                variants={pageVariants}
+                transition={pageTransition}
+                style={{ width: "100%" }}
+              >
+                <ProtectedRoute>
+                  <Detection />
+                </ProtectedRoute>
+              </motion.div>
+            }
+          />
+          <Route
+            path="/history"
+            element={
+              <motion.div
+                initial="initial"
+                animate="in"
+                exit="out"
+                variants={pageVariants}
+                transition={pageTransition}
+                style={{ width: "100%" }}
+              >
+                <ProtectedRoute>
+                  <History />
+                </ProtectedRoute>
+              </motion.div>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <motion.div
+                initial="initial"
+                animate="in"
+                exit="out"
+                variants={pageVariants}
+                transition={pageTransition}
+                style={{ width: "100%" }}
+              >
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              </motion.div>
+            }
+          />
+        </Routes>
+      </AnimatePresence>
+    </>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+}
